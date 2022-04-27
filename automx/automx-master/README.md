d2184765d0ddff9635b69c3025b8b49e1d029538
root <root@mail.pinboxgateway.com>
2022-04-27 11:17:44 +0530

    modoboa

---
+++ automx/automx-master/README.md
@@ -0,0 +1,2 @@
+This project has been deprecated in favor of automx2.
+You can find automx2 on GitHub: https://github.com/rseichter/automx2
---
+++ modoboa/env/bin/modoboa-admin.py
@@ -0,0 +1,6 @@
+#!/srv/modoboa/env/bin/python3
+
+from modoboa.core.commands import handle_command_line
+
+if __name__ == "__main__":
+    handle_command_line()
